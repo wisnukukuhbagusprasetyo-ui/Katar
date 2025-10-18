7995e13c15e778a22f9425a64850849646472aae
wisnukukuhbagusprasetyo-ui <wisnukukuhbagusprasetyo@gmail.com>
2025-10-18 19:51:29 +0700

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,6 @@
-// ====== KONFIGURASI FIREBASE & GOOGLE DRIVE ======
+// ====== KONFIGURASI FIREBASE + IMGBB ======
 
+// --- Firebase Project (Autentikasi + Firestore + Hosting)
 window.__ENV = {
   apiKey: "AIzaSyCgJC8OQBG_wQt57tZHfNuVKPb2VVlAalI",
   authDomain: "karang-taruna-aadaf.firebaseapp.com",
@@ -9,16 +10,7 @@ window.__ENV = {
   appId: "1:367208001887:web:bce5982d99edefb7e746ea"
 };
 
-// (isi nanti kalau sudah aktifkan Google Drive API)
-window.__GDRIVE = {
-  apiKey: "API_KEY_GDRIVE",
-  clientId: "CLIENT_ID.apps.googleusercontent.com",
-  folders: {
-    profil: "FOLDER_ID_PROFIL",
-    galeri: "FOLDER_ID_GALERI",
-    umkm: "FOLDER_ID_UMKM",
-    surat: "FOLDER_ID_SURAT",
-    proposal: "FOLDER_ID_PROPOSAL",
-    laporan: "FOLDER_ID_LAPORAN"
-  }
+// --- ImgBB (Upload Gambar Gratis)
+window.__IMGBB = {
+  apiKey: "d0a0d866377c1d4daa8cd9d937303b76"
 };
