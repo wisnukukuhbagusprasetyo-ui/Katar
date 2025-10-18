0c95d55115bd1669ed56d49ad481a2502c962148
wisnukukuhbagusprasetyo-ui <wisnukukuhbagusprasetyo@gmail.com>
2025-10-18 19:15:17 +0700

    Update Firebase configuration in env.js

--- env.js
+++ env.js
@@ -1,22 +1,24 @@
-// ==== CONFIGURE WITH YOUR REAL KEYS ====
+// ====== KONFIGURASI FIREBASE & GOOGLE DRIVE ======
+
 window.__ENV = {
-  apiKey: "API_KEY_FIREBASE",
-  authDomain: "nama-project.firebaseapp.com",
-  projectId: "nama-project",
-  storageBucket: "nama-project.appspot.com",
-  messagingSenderId: "XXXXXXXXXXXXX",
-  appId: "APP_ID_FIREBASE"
+  apiKey: "AIzaSyCgJC8OQBG_wQt57tZHfNuVKPb2VVlAalI",
+  authDomain: "karang-taruna-aadaf.firebaseapp.com",
+  projectId: "karang-taruna-aadaf",
+  storageBucket: "karang-taruna-aadaf.appspot.com",
+  messagingSenderId: "367208001887",
+  appId: "1:367208001887:web:bce5982d99edefb7e746ea"
 };
 
+// (isi nanti kalau sudah aktifkan Google Drive API)
 window.__GDRIVE = {
   apiKey: "API_KEY_GDRIVE",
   clientId: "CLIENT_ID.apps.googleusercontent.com",
   folders: {
-    profil:   "FOLDER_ID_PROFIL",
-    galeri:   "FOLDER_ID_GALERI",
-    umkm:     "FOLDER_ID_UMKM",
-    surat:    "FOLDER_ID_SURAT",
+    profil: "FOLDER_ID_PROFIL",
+    galeri: "FOLDER_ID_GALERI",
+    umkm: "FOLDER_ID_UMKM",
+    surat: "FOLDER_ID_SURAT",
     proposal: "FOLDER_ID_PROPOSAL",
-    laporan:  "FOLDER_ID_LAPORAN"
+    laporan: "FOLDER_ID_LAPORAN"
   }
 };
