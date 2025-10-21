252bc52bb502a51f896cf14fd817bcb34623c963
wisnukukuhbagusprasetyo-ui <wisnukukuhbagusprasetyo@gmail.com>
2025-10-21 14:42:06 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-
-// js/config.js
-export const UNIT_ID = "cilosari_barat";         // ID Karang Taruna aktif pada portal ini
-export const UNIT_PRIVACY = true;                // true = isolasi per-unit
